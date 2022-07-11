3723828b8d749dd6916b360c0ce03175e0a28275
Yogendra Jhala <yogendrajhala650@gmail.com>
2022-07-11 21:49:52 +0530

    First commit Auth Related

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+const ipfsClient = require('ipfs-http-client');
+
+const projectId = '1qmt...XXX';
+const projectSecret = 'c920...XXX';
+const auth =
+    'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+const client = ipfsClient.create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+        authorization: auth,
+    },
+});
+
+client.pin.add('QmeGAVddnBSnKc1DLE7DLV9uuTqo5F7QbaveTjr45JUdQn').then((res) => {
+    console.log(res);
+});
 
 function App() {
   return (
